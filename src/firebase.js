72b73c55a75d8f14f86cb416db2ceceb15cda685
Tamericanos <sistemas@transportesamericanos.com>
2026-08-03 12:18:43 -0600

    actualizacion firerules

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,13 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
-// TODO: Reemplazar con la configuración de Firebase de la consola
 const firebaseConfig = {
-  apiKey: "TU_API_KEY",
+  apiKey: "AIzaSyAdAbbGB1LjKTIqblQcqQDg0SnmCZpbqbw",
   authDomain: "studio-3644374455-4c9fb.firebaseapp.com",
   projectId: "studio-3644374455-4c9fb",
-  storageBucket: "studio-3644374455-4c9fb.appspot.com",
-  messagingSenderId: "TU_SENDER_ID",
-  appId: "TU_APP_ID"
+  storageBucket: "studio-3644374455-4c9fb.firebasestorage.app",
+  messagingSenderId: "228851074201",
+  appId: "1:228851074201:web:9da0f867e3f8245bb86408"
 };
 
 const app = initializeApp(firebaseConfig);
